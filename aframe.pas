c1d13ac6536fecb59c983d6739c4e275e00d51e5
Christian U <info@cu-tec.de>
2019-01-03 22:52:10 +0100

    initial working version

--- aframe.pas
+++ aframe.pas
@@ -8,6 +8,32 @@ interface
 uses
   js,web;
 
+type
+  TJSAFRAME = Class external name 'AFRAME'
+  Public
+    function AComponent(arg1: JSValue;arg2: JSValue;arg3: JSValue) : JSValue;
+    components : TJSObject;
+    geometries : TJSObject;
+    function registerComponent(arg1: JSValue;arg2: JSValue) : JSValue;
+    function registerElement(arg1: JSValue;arg2: JSValue) : JSValue;
+    function registerGeometry(arg1: JSValue;arg2: JSValue) : JSValue;
+    function registerPrimitive(arg1: JSValue;arg2: JSValue) : JSValue;
+    function registerShader(arg1: JSValue;arg2: JSValue) : JSValue;
+    function registerSystem(arg1: JSValue;arg2: JSValue) : JSValue;
+    primitives : TJSObject;
+    scenes : TJSObject;
+    schema : TJSObject;
+    shaders : TJSObject;
+    systems : TJSObject;
+    THREE : TJSObject;
+    TWEEN : TJSObject;
+    utils : TJSObject;
+    version : string;
+  end;
+
+var
+  AFRAME : TJSAFRAME external name 'AFRAME';
+
 implementation
 
 end.
--- demos/minimal/minimal.pas
+++ demos/minimal/minimal.pas
@@ -3,6 +3,6 @@ program minimal;
 uses sysutils, js, class2pas, aframe;
 
 begin
-  ;
+  aframe.AFRAME.registerComponent(null,null);
 end.
 
